890ccf0a138f51367a28d4c45a0aabee7ca80452
dssdf <dsds@126.com>
2020-12-31 09:50:43 +0800

    初始化

---
+++ pages/test/test.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		测试
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+			console.log('test1')
+		},
+		onTabItemTap(e) {
+			// this.$router.push(e.pagePath)
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/test2/test2.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+			console.log('test2')
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
